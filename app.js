0a27175ee3f1dadf204c9a935ba4afa50b527984
Steffan Crowley <steffancrow@gmail.com>
2022-05-05 13:34:30 -0700

    Initial project setup, link css,html, and js

---
+++ app.js
@@ -0,0 +1,9 @@
+const container = document.querySelector("#container");
+
+const square = document.createElement("div");
+
+square.classList.add("square");
+
+square.textContent = "This is the glorious text-content!";
+
+container.appendChild(square);
