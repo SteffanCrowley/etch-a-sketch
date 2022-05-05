ccf3099f9f45c2adf3599b685d23d6d2c9035fde
Steffan Crowley <steffancrow@gmail.com>
2022-05-05 15:35:51 -0700

    Create entire 16 x 16 grid

--- app.js
+++ app.js
@@ -1,15 +1,18 @@
 const container = document.querySelector("#container");
 
 const square = [];
+let rows = 16;
 
-createGrid();
+createGrid(rows);
 
-function createGrid() {
-  for (let i = 0; i < 16; i++) {
-    square[0] = document.createElement("div");
+function createGrid(rows) {
+  for (let x = 0; x < rows; x++) {
+    for (let i = 0; i < rows; i++) {
+      square[0] = document.createElement("div");
 
-    square[0].classList.add("square");
+      square[0].classList.add("square");
 
-    container.append(square[0]);
+      container.append(square[0]);
+    }
   }
 }
